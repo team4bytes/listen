07ae4742a77e11d666164eaa75cec6f6a0b3b676
team4bytes <satishkumarsahoo@gmail.com>
2018-03-10 12:44:18 +0530

    test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule, Routes } from '@angular/router';
-import { ProductDetailComponent } from './product-detail/product-detail.component';
+// import { ProductDetailComponent } from './product-detail/product-detail.component';
 import { ProductsComponent } from './products/products.component';
 
 const routes: Routes = [
   { path: '', redirectTo: '/products', pathMatch: 'full' },
-    { path: 'detail/:id', component: ProductDetailComponent },
+    // { path: 'detail/:id', component: ProductDetailComponent },
   { path: 'products', component: ProductsComponent }
 ];
  
